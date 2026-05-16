00cef603d2651bd61679eff79fad805c8f7a65f1
Peron <peron_ap@icloud.com>
2026-05-17 04:41:58 +0800

    chore: use shared TIMEOUT

--- src/pageLoader.ts
+++ src/pageLoader.ts
@@ -76,7 +76,7 @@ function eagerLazy(): void {
 
 async function waitForCaptcha(page: Page): Promise<void> {
 	if (await page.$('#anubis_challenge')) {
-		await page.waitForSelector('#anubis_challenge', { state: 'detached', timeout: 30000 });
+		await page.waitForSelector('#anubis_challenge', { state: 'detached', timeout: TIMEOUT });
 		await page.waitForLoadState('networkidle', { timeout: TIMEOUT });
 	}
 }
